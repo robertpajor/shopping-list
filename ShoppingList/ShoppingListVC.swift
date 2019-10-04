9ac5dc8bc7e3773cd4aef47421fe734a20ba8a3e
Robert Pajor <pajor.robert@gmail.com>
2019-10-04 11:21:48 +0200

    Add alert for adding shopping list

--- ShoppingList/ShoppingListVC.swift
+++ ShoppingList/ShoppingListVC.swift
@@ -8,21 +8,53 @@
 
 import UIKit
 
-class ShoppingListVC: UITableViewController {
+class ShoppingListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    
+    @IBAction func clickAddButton(_ sender: Any) {
+        showAddAlert()
+    }
+
+    func showAddAlert() {
+        let addAlert = UIAlertController(title: "", message: "", preferredStyle: .alert)
+
+        let attributedString = NSAttributedString(string: "Add new list", attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17),
+            NSAttributedString.Key.foregroundColor: UIColor(named: "titleGrey")!
+            ])
+        addAlert.setValue(attributedString, forKey: "attributedTitle")
+
+        addAlert.addTextField { (textField) in
+            textField.placeholder = "List name"
+        }
+
+        addAlert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        addAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak addAlert] (_) in
+            let textField = addAlert?.textFields![0]
+            let textFieldText = textField!.text
+            print(textFieldText!)
+        }))
+        addAlert.view.tintColor = UIColor(named: "subtitleGrey")
+        self.present(addAlert, animated: true)
+    }
+
+//    private func getAttributedString(for text: String) -> NSAttributedString {
+//        let attributedString = NSAttributedString(string: text, attributes: [
+//            NSAttributedString.Key.foregroundColor: UIColor(named: "titleGrey")
+//            ])
+//        return attributedString
+//    }
+
+
+
+    
     // MARK: - Table view data source
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+/*    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 20
     }
@@ -34,52 +66,5 @@ class ShoppingListVC: UITableViewController {
         // Configure the cell...
 
         return cell
-    }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    }*/
 }
