098370160243b430c73fb4033534148b475c4adc
Robert Pajor <46776400+robertpajor@users.noreply.github.com>
2019-10-26 09:48:48 +0200

    "Add new list" popup (#1)
    * Add alert for adding shopping list
    
    * Move attributedString to private method
    
    * Refactor showAddAlert method
    
    * Create alert extension
    
    * Clean guard let
    
    * Refactor closures

--- ShoppingList/ShoppingListVC.swift
+++ ShoppingList/ShoppingListVC.swift
@@ -8,78 +8,29 @@
 
 import UIKit
 
-class ShoppingListVC: UITableViewController {
+class ShoppingListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
-    // MARK: - Table view data source
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 20
+    @IBAction func clickAddButton(_ sender: Any) {
+        showAddAlert()
     }
 
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    func showAddAlert() {
+        let addAlert = UIAlertController.make()
+        addAlert.setTitle(withText: "Add new list")
+        addAlert.addField(withPlaceholder: "List name")
+        addAlert.addCancelButton(withHandler: nil)
+        addAlert.addOkButton { [weak self] text in
+            self?.printText(text)
+        }
+        self.present(addAlert, animated: true)
     }
 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+// It is temporary function
+    func printText(_ text: String) {
+        print(String(format: "Text from field: %@", text))
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ ShoppingList/UIAlertController.swift
@@ -0,0 +1,44 @@
+//
+//  UIAlertController.swift
+//  ShoppingList
+//
+//  Created by Robert Pajor on 14/10/2019.
+//  Copyright © 2019 Robert Pajor. All rights reserved.
+//
+
+import UIKit
+
+extension UIAlertController {
+
+    static func make() -> UIAlertController {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        alert.view.tintColor = .secondaryGrey
+        return alert
+    }
+
+    func setTitle(withText title: String) {
+        let attributedString = NSAttributedString(string: title, attributes: [
+            NSAttributedString.Key.foregroundColor: UIColor.primaryGrey!
+        ])
+        self.setValue(attributedString, forKey: "attributedTitle")
+    }
+
+    func addField(withPlaceholder placeholder: String) {
+        self.addTextField { textField in
+            textField.placeholder = placeholder
+        }
+    }
+
+    func addCancelButton(withHandler handler: (() -> Void)?) {
+        self.addAction(UIAlertAction(title: "Cancel", style: .cancel) { (_) in
+            handler?()
+        })
+    }
+
+    func addOkButton(withHandler handler: ((String) -> Void)?) {
+        self.addAction(UIAlertAction(title: "OK", style: .default) { [weak self] (_) in
+            let text = self?.textFields?[0].text ?? ""
+            handler?(text)
+        })
+    }
+}
